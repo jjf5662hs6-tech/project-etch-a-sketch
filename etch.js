aad78bf248e4ddc9ddc41125beca1d5f5168a646
jjf5662hs6-tech <250735204+jjf5662hs6-tech@users.noreply.github.com>
2026-01-31 18:03:41 -0500

    added 2 new features, random color and opacity darkening effect

--- etch.js
+++ etch.js
@@ -4,10 +4,22 @@ const btn = document.querySelector("#btn");
 for (let i = 0; i < 16**2; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.opacity = 0.1;
     containerDiv.appendChild(square);
     square.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
-    });
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+        let currentOpacity = parseFloat(square.style.opacity);
+        let newOpacity = currentOpacity + 0.1;
+        square.style.opacity = newOpacity;
+        
+        if (newOpacity > 1) {newOpacity = 1} ;
+
+    });    
 }
 
 // btn.addEventListener("click", () => {
@@ -29,6 +41,12 @@ function createGrid(size) {
     let userInput =  prompt("Enter number of squares per side for your new grid");
         size = Number(userInput);
 
+        if (userInput === null) { return }
+        
+        else if ( userInput > 100 || userInput < 0 || userInput === "" || isNaN(userInput) ) {
+            return alert("Invalid entry");
+        }
+
     containerDiv.replaceChildren();
 
     for (let i = 0; i < size**2; i++) {
@@ -36,11 +54,22 @@ function createGrid(size) {
     square.classList.add("square");
     square.style.width = `${100 / size}%`;
     square.style.height = `${100 / size}%`;
+    square.style.opacity = 0.1;
     containerDiv.appendChild(square);
     square.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+        let currentOpacity = parseFloat(square.style.opacity);
+        let newOpacity = currentOpacity + 0.1;
+        square.style.opacity = newOpacity;
     });
-}
+    }
 };
 
 btn.addEventListener("click", createGrid);
+
+
